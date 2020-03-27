381ce998ec76b2c34843cce975b884b574e88a71
Roman A <andreenkorv@yandex.ru>
2020-03-27 16:55:54 +0300

    parse habr news in print

--- webapp/news/parsers/utils.py
+++ webapp/news/parsers/utils.py
@@ -1,11 +1,25 @@
 import requests
 
+from webapp.db import db
+from webapp.news.models import News
+
 
 def get_html(url):
+    headers = {
+         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0'
+        }
     try:
-        result = requests.get(url)
+        result = requests.get(url, headers=headers)
         result.raise_for_status()
         return result.text
     except(requests.RequestException, ValueError):
         print("Сетевая ошибка")
         return False
+
+
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    if not news_exists:
+        new_news = News(title=title, url=url, published=published)
+        db.session.add(new_news)
+        db.session.commit()
