6040eb83f42c9cf62bce4b2299d2d6a13b607628
Roman A <andreenkorv@yandex.ru>
2020-03-31 14:38:52 +0300

    Добавлен celery+crontab

--- webapp/news/parsers/habr.py
+++ webapp/news/parsers/habr.py
@@ -1,8 +1,30 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+import locale
+import platform
 
 from bs4 import BeautifulSoup
 
+from webapp.db import db
+from webapp.news.models import News
 from webapp.news.parsers.utils import get_html, save_news
+
+if platform.system() == "Windows":
+    locale.setlocale(locale.LC_ALL, "russian")
+else:
+    locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
+
+
+def parse_habr_date(date_str):
+    if 'сегодня' in date_str:
+        today = datetime.now()
+        date_str = date_str.replace('сегодня', today.strftime('%d %B %Y'))
+    elif 'вчера' in date_str:
+        yesterday = datetime.now() - timedelta(days=1)
+        date_str = date_str.replace('вчера', yesterday.strftime('%d %B %Y'))
+    try:
+        return datetime.strptime(date_str, '%d %B %Y в %H:%M')
+    except ValueError:
+        return datetime.now()
 
 
 def get_habr_snippets():
@@ -14,12 +36,18 @@ def get_habr_snippets():
             title = news.find('a', class_='post__title_link').text
             url = news.find('a', class_='post__title_link')['href']
             published = news.find('span', class_='post__time').text
-            print(title, url, published)
-            '''
-            try:
-                published = datetime.strptime(published, '%Y-%m-%d')
-            except ValueError:
-                published = datetime.now()
+            published = parse_habr_date(published)
             save_news(title, url, published)
-            '''
-    return 0
+
+
+def get_news_content():
+    news_without_text = News.query.filter(News.text.is_(None))
+    for news in news_without_text:
+        html = get_html(news.url)
+        if html:
+            soup = BeautifulSoup(html, 'html.parser')
+            news_text = soup.find('div', class_='post__text-html').decode_contents()
+            if news_text:
+                news.text = news_text
+                db.session.add(news)
+                db.session.commit()
--- webapp/news/views.py
+++ webapp/news/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import abort, Blueprint, render_template
 from webapp.news.models import News, Weather
 
 blueprint = Blueprint('news', __name__)
@@ -9,7 +9,18 @@ def index():
     page_title = "Новости Python"
     weather_list = Weather.query.all()
     news_list = News.query.order_by(News.published.desc()).all()
+    news_list = News.query.filter(News.text.isnot(None)).order_by(News.published.desc()).all()
     return render_template(
         'news/index.html', page_title=page_title, news_list=news_list,
         weather_list=weather_list
         )
+
+
+@blueprint.route('/news/<int:news_id>')
+def single_news(news_id):
+    my_news = News.query.filter(News.id == news_id).first()
+
+    if not my_news:
+        abort(404)
+
+    return render_template('news/single_news.html', page_title=my_news.title, news=my_news)
